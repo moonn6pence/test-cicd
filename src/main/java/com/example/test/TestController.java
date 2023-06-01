b8cf557fa3315d503ab80adabba35430017e0436
moonn6pence <shin421179@gmail.com>
2023-06-01 22:20:19 +0900

    test push 2
    .

--- src/main/java/com/example/test/TestController.java
+++ src/main/java/com/example/test/TestController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @GetMapping("/")
+    public String getDefault(){
+        return "Hello CI/CD!";
+    }
+
     @GetMapping("/hello")
     public String getHello(){
         return "Hello Github Actions!";
